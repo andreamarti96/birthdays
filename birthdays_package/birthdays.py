935158aac7b139c4131df0963489b762eebcba52
francescomenazza96 <francesco.menazza@student.h-farm.com>
2019-12-11 12:06:37 -0500

    documented birthdays.py

--- birthdays_package/birthdays.py
+++ birthdays_package/birthdays.py
@@ -1,3 +1,7 @@
+'''
+Return the birthday of a given person
+'''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +10,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    '''Print all the names of the people in the birthdays dictionaries'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    '''If the name is in the birthdays dictionaries print the birthday otherwise print a warning message''' 
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
