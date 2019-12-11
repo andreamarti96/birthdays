e5f42a9cf8836b0537d68e56a99a4d6287113b9c
EdoardoDone <edoardo.done01@gmail.com>
2019-12-11 18:28:11 +0100

    Implemented sys.argv in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python3
 
+import sys
 from birthdays_package.birthdays import return_birthday
 
-return_birthday('Albert Einstein')
-return_birthday('Alan Turing')
+if len(sys.argv) == 3:
+    user_input = sys.argv[1] + " " + sys.argv[2]
+else:
+    print("Enter: name username")
+    exit()
+
+return_birthday(user_input)
