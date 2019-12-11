78f594c3da066366c05a867f30fe2a9cbcf30fe4
isac13 <isacventurin@gmail.com>
2019-12-11 18:34:12 +0100

    fixed coding style in birthdays.py

--- birthdays_package/birthdays.py
+++ birthdays_package/birthdays.py
@@ -9,16 +9,19 @@ birthdays = {
     'Donald Trump': '06/14/1946',
     'Rowan Atkinson': '01/6/1955'}
 
+
 def print_birthdays():
     '''Print all the names of the people in the birthdays dictionaries'''
-    print('Welcome to the birthday dictionary. We know the birthdays of these people:')
+    print('Welcome to the birthday dictionary. \
+We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
+
 def return_birthday(name):
-    '''If the name is in the birthdays dictionaries print the birthday otherwise print a warning message''' 
+    '''If the name is in the birthdays dictionaries \
+print the birthday otherwise print a warning message'''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
         print('Sadly, we don\'t have {}\'s birthday.'.format(name))
-
