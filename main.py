942608f0ea669a7ccb8dada7ab9d42f8e735b864
francescomenazza96 <francesco.menazza@student.h-farm.com>
2020-01-14 11:22:40 -0500

    Implement argparse into main file

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-#! /usr/bin/env python3
+import argparse
+from birthdays_package.birthdays_module import return_data,
+return_index, return_set
 
-import sys
-from birthdays_package.birthdays import return_birthday
+'''Given a name, it returns his/her birth day, death, homwtown an
+name of the people born in the same century, according to different
+level of verbosity'''
 
-if len(sys.argv) == 3:
-    user_input = sys.argv[1] + " " + sys.argv[2]
-else:
-    print("Enter: name username")
-    exit()
 
-return_birthday(user_input)
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="return the birthday", type=str)
+    parser.add_argument("-v", "--verbosity", help="increase output verbosity",
+                        action="count")
+    parser.add_argument("-u", "--set", help="group the birthdays",
+                        action="store_true")
+
+    args = parser.parse_args()
+    i = return_index(args.name)
+    dataset = return_data()
+    if i is None:
+        print("error")
+    else:
+        if args.verbosity > 1:
+            print('The '+args.name.upper() + '\'s' + ' birthday is: ' +
+                  dataset[1][i])
+            print('The '+args.name.upper() + '\'s' + ' death is: ' +
+                  dataset[2][i])
+            print('The '+args.name.upper() + '\'s' + ' hometown is: ' +
+                  dataset[3][i])
+        elif args.verbosity == 1:
+            print('The ' + args.name.upper() + '\'s' + ' birthday is: ' +
+                  dataset[1][i])
+            print('The ' + args.name.upper() + '\'s' + ' death is: ' +
+                  dataset[2][i])
+        elif args.set:
+            return_set(args.name)
+        else:
+            print('The '+args.name.upper() + '\'s' + ' birthday is: ' +
+                  dataset[1][i])
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
